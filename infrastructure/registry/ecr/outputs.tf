f8d069403d47ab8d8352182fb488f77389c438bc
dcreutz <dennis.creutz@prodyna.com>
2022-07-15 11:49:13 +0200

    Fix ecr outputs

--- infrastructure/registry/ecr/outputs.tf
+++ infrastructure/registry/ecr/outputs.tf
@@ -1,7 +1,7 @@
-output "order_manager_image_url" {
+output "config_service_image_url" {
   value = module.ecr_config_service.repository_url
 }
 
-output "order_manager_repo_name" {
+output "config_service_repo_name" {
   value = module.ecr_config_service.repository_name
 }
